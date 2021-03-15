7abebc4203eeea25ce394df6880ab99e3daa4693
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-03-15 12:53:39 +0100

    Importaciones necesarias

--- src/Suma_Funciones_Pares.lean
+++ src/Suma_Funciones_Pares.lean
@@ -3,12 +3,14 @@ import algebra.group.pi
 
 def even_fun (f : ℝ → ℝ) := ∀ x, f (-x) = f x
 
-example (f g : ℝ → ℝ) : even_fun f → even_fun g →  even_fun (f + g) :=
+example (
+  f g : ℝ → ℝ)
+  : even_fun f → even_fun g →  even_fun (f + g) :=
 begin
   intros hf hg,
   intros x,
   calc (f + g) (-x) = f (-x) + g (-x) : rfl
-                ... = f x + g (-x)    : by rw hf 
-                ... = f x + g x       : by rw hg 
+                ... = f x + g (-x)    : by rw hf
+                ... = f x + g x       : by rw hg
                 ... = (f + g) x       : rfl
 end
