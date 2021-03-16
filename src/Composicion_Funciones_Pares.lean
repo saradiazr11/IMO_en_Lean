8d4d696016ffa789406ddcb3e7745475f0a10f67
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-03-16 16:46:28 +0100

    Eliminación de importaciones no necesarias.

--- src/Composicion_Funciones_Pares.lean
+++ src/Composicion_Funciones_Pares.lean
@@ -1,12 +1,11 @@
 import data.real.basic
--- import algebra.group.pi
 
 def even_fun (f : ℝ → ℝ) := ∀ x, f (-x) = f x
 
 example (f g : ℝ → ℝ) : even_fun f → even_fun (g ∘ f) :=
 begin
   intros hf x,
-  calc (g ∘ f) (-x) 
+  calc (g ∘ f) (-x)
       = g (f (-x)) : rfl
   ... = g (f x)    : by rw hf,
 end
