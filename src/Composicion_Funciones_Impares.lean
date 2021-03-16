dbed06107171ab9eef689d5a68766ee7903a0786
saradiazr11 <saradiazr11@gmail.com>
2021-03-16 11:49:12 +0100

    Correcciones modificadas del 15-Marzo

--- src/Composicion_Funciones_Impares.lean
+++ src/Composicion_Funciones_Impares.lean
@@ -1,5 +1,5 @@
 import data.real.basic
-import algebra.group.pi
+-- import algebra.group.pi
 
 def odd_fun (f : ℝ → ℝ) := ∀ x, f (-x) = -f x
 
--- src/Composicion_Funciones_Pares.lean
+++ src/Composicion_Funciones_Pares.lean
@@ -1,5 +1,5 @@
 import data.real.basic
-import algebra.group.pi
+-- import algebra.group.pi
 
 def even_fun (f : ℝ → ℝ) := ∀ x, f (-x) = f x
 
