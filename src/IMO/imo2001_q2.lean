cab166d9c2ac54cee3021844943c20024b64aa54
saradiazr11 <saradiazr11@gmail.com>
2021-05-01 19:10:19 +0200

    Actualizacion TFM

--- src/IMO/imo2001_q2.lean
+++ src/IMO/imo2001_q2.lean
@@ -1,34 +1,38 @@
-/-
-Copyright (c) 2021 Tian Chen. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Tian Chen
--/
-
 import analysis.special_functions.pow
 
 /-!
-# IMO 2001 Q2
-Let $a$, $b$, $c$ be positive reals. Prove that
-$$
+Sean a, b y c tres números reales y positivos. Demostrar que
 \frac{a}{\sqrt{a^2 + 8bc}} +
 \frac{b}{\sqrt{b^2 + 8ca}} +
 \frac{c}{\sqrt{c^2 + 8ab}} ≥ 1.
-$$
-## Solution
-This proof is based on the bound
-$$
-\frac{a}{\sqrt{a^2 + 8bc}} ≥
-\frac{a^{\frac43}}{a^{\frac43} + b^{\frac43} + c^{\frac43}}.
-$$
 -/
 
 open real
 
 variables {a b c : ℝ}
 
-lemma denom_pos (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
+lemma suma_pos (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
   0 < a ^ 4 + b ^ 4 + c ^ 4 :=
-add_pos (add_pos (pow_pos ha 4) (pow_pos hb 4)) (pow_pos hc 4)
+begin
+  have ha1: 0 < a ^ 4:=pow_pos ha 4,
+  have hb1: 0 < b ^ 4:=pow_pos hb 4,
+  have hc1: 0 < c ^ 4:=pow_pos hc 4,
+  have sum1: 0< a ^ 4 + b ^ 4 := add_pos ha1 hb1,
+  have sum2: 0< a ^ 4 + b ^ 4 + c ^ 4 := add_pos sum1 hc1,
+  exact sum2,
+end
+/--/
+lemma cota (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
+  a ^ 4 / (a ^ 4 + b ^ 4 + c ^ 4) ≤
+  a ^ 3 / sqrt ((a ^ 3) ^ 2 + 8 * b ^ 3 * c ^ 3) :=
+begin
+  have ha3: 0 < a ^ 3:= pow_pos ha 3,
+  have ha32: 0 < (a ^ 3) ^ 2:= pow_pos ha3 2,
+  have hb8: 0 < 8 * b ^ 3 := mul_pos (bit0_pos zero_lt_four) (pow_pos hb 3),
+  have hsqrt: 0 < 8 * b ^ 3 * c ^ 3 := mul_pos hb8 (pow_pos hc 3),
+  have hdenom := suma_pos ha hb hc,
+  
+end-/
 
 lemma bound (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
   a ^ 4 / (a ^ 4 + b ^ 4 + c ^ 4) ≤
@@ -36,12 +40,12 @@ lemma bound (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
 begin
   have hsqrt := add_pos_of_nonneg_of_pos (pow_two_nonneg (a ^ 3))
     (mul_pos (mul_pos (bit0_pos zero_lt_four) (pow_pos hb 3)) (pow_pos hc 3)),
-  have hdenom := denom_pos ha hb hc,
+  have hdenom := suma_pos ha hb hc,
   rw div_le_div_iff hdenom (sqrt_pos.mpr hsqrt),
   conv_lhs { rw [pow_succ', mul_assoc] },
   apply mul_le_mul_of_nonneg_left _ (pow_pos ha 3).le,
   apply le_of_pow_le_pow _ hdenom.le zero_lt_two,
-  rw [mul_pow, sqr_sqrt hsqrt.le, ← sub_nonneg],
+  rw [mul_pow, sq_sqrt hsqrt.le, ← sub_nonneg],
   calc  (a ^ 4 + b ^ 4 + c ^ 4) ^ 2 - a ^ 2 * ((a ^ 3) ^ 2 + 8 * b ^ 3 * c ^ 3)
       = 2 * (a ^ 2 * (b ^ 2 - c ^ 2)) ^ 2 + (b ^ 4 - c ^ 4) ^ 2 +
         (2 * (a ^ 2 * b * c - b ^ 2 * c ^ 2)) ^ 2 : by ring
@@ -58,7 +62,7 @@ have h₁ : b ^ 4 + c ^ 4 + a ^ 4 = a ^ 4 + b ^ 4 + c ^ 4,
 have h₂ : c ^ 4 + a ^ 4 + b ^ 4 = a ^ 4 + b ^ 4 + c ^ 4,
   by rw [add_assoc, add_comm],
 calc _ ≥ _ : add_le_add (add_le_add (bound ha hb hc) (bound hb hc ha)) (bound hc ha hb)
-   ... = 1 : by rw [h₁, h₂, ← add_div, ← add_div, div_self $ ne_of_gt $ denom_pos ha hb hc]
+   ... = 1 : by rw [h₁, h₂, ← add_div, ← add_div, div_self $ ne_of_gt $ auma_pos ha hb hc]
 
 theorem imo2001_q2 (ha : 0 < a) (hb : 0 < b) (hc : 0 < c) :
   1 ≤ a / sqrt (a ^ 2 + 8 * b * c) +
