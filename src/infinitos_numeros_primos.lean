38c0f95598bf23b4c86f778878de06ce601fe79e
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-06-02 18:46:55 +0200

    Revisión del 2-jun-21

--- src/infinitos_numeros_primos.lean
+++ src/infinitos_numeros_primos.lean
@@ -3,14 +3,17 @@ import data.nat.prime
 
 open nat
 
+-- Para que no use la notación con puntos
+set_option pp.structure_projections false
+
 theorem infinitos_numeros_primos :
   ∀ n, ∃ p ≥ n, prime p:=
 begin
   intro n,
 
-  let m:= factorial n + 1,
-  let p:= min_fac m,
-  have hp: prime p :=
+  let m := factorial n + 1,
+  let p := min_fac m,
+  have hp : prime p :=
   begin
     refine min_fac_prime _,
     sorry,
@@ -19,32 +22,30 @@ begin
   use p,
   split,
   { by_contradiction,
-    have h1: p ∣ factorial n + 1 := min_fac_dvd m,
-    have h2: p ∣ factorial n := by sorry,
-    have h3: p ∣ 1 := (nat.dvd_add_right h2).mp h1,
+    have h1 : p ∣ factorial n + 1 := min_fac_dvd m,
+    have h2 : p ∣ factorial n := by sorry,
+    have h3 : p ∣ 1 := (nat.dvd_add_right h2).mp h1,
     exact prime.not_dvd_one hp h3,},
   { exact hp, },
 end
 
 lemma h2
   (n : ℕ)
-  (m : ℕ)
-  (p : ℕ)
-  (m = n.factorial + 1)
-  (p = m.min_fac)
+  (m = factorial n + 1)
+  (p = min_fac m)
   (hp : prime p)
   (h : ¬p ≥ n)
-  (h1 : p ∣ n.factorial + 1)
+  (h1 : p ∣ factorial n + 1)
   : p ∣ factorial n:=
 begin
-  refine hp.dvd_factorial.mpr _,
+  refine (prime.dvd_factorial hp).mpr _,
   exact le_of_not_ge h,
 end
 
 lemma hp
   (n : ℕ)
-  (m = n.factorial + 1)
-  (p = m.min_fac)
+  (m = factorial n + 1)
+  (p = min_fac m)
   : prime p:=
 begin
   sorry,
