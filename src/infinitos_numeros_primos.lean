69ebd14ef2af7ad3a3024ac445d9cd7936285a11
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-06-02 17:14:37 +0200

    infinitos_numeros_primos: Reformatear.

--- src/infinitos_numeros_primos.lean
+++ src/infinitos_numeros_primos.lean
@@ -1,10 +1,10 @@
 import data.nat.factorial
 import data.nat.prime
 
-
 open nat
 
-theorem infinitos_numeros_primos : ∀ n, ∃ p ≥ n, prime p:=
+theorem infinitos_numeros_primos :
+  ∀ n, ∃ p ≥ n, prime p:=
 begin
   intro n,
 
@@ -23,29 +23,29 @@ begin
     have h2: p ∣ factorial n := by sorry,
     have h3: p ∣ 1 := (nat.dvd_add_right h2).mp h1,
     exact prime.not_dvd_one hp h3,},
-  {exact hp,},
+  { exact hp, },
 end
 
 lemma h2
-(n: ℕ)
-(m: ℕ)
-(p: ℕ)
-(m = n.factorial + 1)
-(p = m.min_fac)
-(hp: prime p)
-(h: ¬p ≥ n)
-(h1: p ∣ n.factorial + 1)
-: p ∣ factorial n:=
+  (n : ℕ)
+  (m : ℕ)
+  (p : ℕ)
+  (m = n.factorial + 1)
+  (p = m.min_fac)
+  (hp : prime p)
+  (h : ¬p ≥ n)
+  (h1 : p ∣ n.factorial + 1)
+  : p ∣ factorial n:=
 begin
   refine hp.dvd_factorial.mpr _,
   exact le_of_not_ge h,
 end
 
-  lemma hp
-  (n: ℕ)
-  (m= n.factorial + 1)
+lemma hp
+  (n : ℕ)
+  (m = n.factorial + 1)
   (p = m.min_fac)
   : prime p:=
-  begin
-    sorry,
-  end
+begin
+  sorry,
+end
