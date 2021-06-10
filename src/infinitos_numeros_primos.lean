3d6397e6cbce645b307a4a6bc31d5899f2be2178
saradiazr11 <saradiazr11@gmail.com>
2021-06-10 09:19:45 +0200

    ACTUALIZACIÓN: Formalización de las demostraciones con Lean

--- src/infinitos_numeros_primos.lean
+++ src/infinitos_numeros_primos.lean
@@ -7,24 +7,26 @@ open nat
 -- Para que no use la notación con puntos
 set_option pp.structure_projections false
 
-theorem infinitos_numeros_primos :
+
+
+theorem infinitos_numeros_primos:
   ∀ n, ∃ p ≥ n, prime p:=
 begin
   intro n,
 
   let m := factorial n + 1,
   let p := min_fac m,
-  have hp : prime p :=
-  begin
-    refine min_fac_prime _,
-    sorry,
-  end,
+  have hp : prime p ,
+  { refine min_fac_prime _,
+    have : 0 < factorial n := factorial_pos n,
+    linarith,},
 
   use p,
   split,
   { by_contradiction,
     have h1 : p ∣ factorial n + 1 := min_fac_dvd m,
-    have h2 : p ∣ factorial n := by sorry,
+    have h2 : p ∣ factorial n,
+    exact iff.mpr (prime.dvd_factorial hp) (le_of_not_ge h),
     have h3 : p ∣ 1 := (nat.dvd_add_right h2).mp h1,
     exact prime.not_dvd_one hp h3,},
   { exact hp, },
@@ -45,12 +47,13 @@ end
 
 lemma hp
   (n m p : ℕ)
-  (h1 : m = factorial n + 1)
-  (h2 : p = min_fac m)
+  (h1p : m = factorial n + 1)
+  (h2p : p = min_fac m)
   : prime p :=
 begin
-  rw h2,
+  rw h2p,
   refine min_fac_prime _,
   have : 0 < factorial n := factorial_pos n,
   linarith,
 end
+
