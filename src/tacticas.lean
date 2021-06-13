7a355cdaa94b560d3094257bec26dbf29501e99a
saradiazr11 <saradiazr11@gmail.com>
2021-06-13 19:45:34 +0200

    Actualización TFM: Avance apéndice tácticas

--- src/tacticas.lean
+++ src/tacticas.lean
@@ -14,6 +14,10 @@ begin
   sorry,
 end
 
+/-
+TÁCTICA SORRY Y HAVE
+-/
+
 example (a b : ℝ) : a = a*b → a = 0 ∨ b = 1 :=
 begin
   intro hyp,
@@ -39,6 +43,10 @@ begin
   rw add_zero (a*a), 
 end
 
+/-
+TÁCTICAS REWRITE Y EXACT
+-/
+
 example (a b c d : ℝ) (hyp : c = d*a + b) (hyp' : b = a*d) : c = 2*a*d :=
 begin
   rw hyp' at hyp,
@@ -49,10 +57,30 @@ begin
 end
 
 /-
+TÁCTICA EXACT
+-/
+
+
+
+/-
 TÁCTICA RING
 -/
 
 example (a b : ℝ) : (a + b) + a = 2*a + b :=
 begin
   by ring,
+end
+
+/-
+TÁCTICA LINARITH
+-/
+def up_bounds (A : set ℝ) := { x : ℝ | ∀ a ∈ A, a ≤ x}
+def is_max (a : ℝ) (A : set ℝ) := a ∈ A ∧ a ∈ up_bounds A
+infix ` is_a_max_of `:55 := is_max
+
+example (A : set ℝ) (x y : ℝ) (hx : x is_a_max_of A) (hy : y is_a_max_of A) : x = y :=
+begin
+  have : x ≤ y, from hy.2 x hx.1,
+  have : y ≤ x, from hx.2 y hy.1,
+  linarith,
 end
