29f9d49e9ba15c0f6619a0b119d09c001425708a
saradiazr11 <saradiazr11@gmail.com>
2021-06-14 22:41:32 +0200

    Actualización TFM: Avance apéndice tácticas

--- src/tacticas.lean
+++ src/tacticas.lean
@@ -20,7 +20,6 @@ TÁCTICA SORRY Y HAVE
 
 example (a b : ℝ) : a = a*b → a = 0 ∨ b = 1 :=
 begin
-  intro hyp,
   have H : a*(1 - b) = 0, by sorry,
   sorry,
 end
@@ -57,6 +56,37 @@ begin
 end
 
 /-
+TÁCTICAS INTRO e INTROS
+-/
+example (a b : ℝ): 0 ≤ b → a ≤ a + b :=
+begin
+  intro hb,
+  exact le_add_of_nonneg_right hb,
+end
+
+example (P Q : Prop) (h₁ : P ∨ Q) (h₂ : ¬ (P ∧ Q)) : Q → ¬P :=
+begin
+  intros q p,
+  exact h₂ ⟨ p,q⟩ ,
+end
+
+/-
+TÁCTICA APPLY
+-/
+def non_decreasing (f : ℝ → ℝ) := ∀ x₁ x₂, x₁ ≤ x₂ → f x₁ ≤ f x₂
+def non_increasing (f : ℝ → ℝ) := ∀ x₁ x₂, x₁ ≤ x₂ → f x₁ ≥ f x₂
+
+example (f g : ℝ → ℝ) (hf : non_decreasing f) (hg : non_increasing g) : 
+non_increasing (g ∘ f) :=
+begin
+  intros x1 x2 h,
+  apply hg,
+  apply hf,
+  exact h,
+end
+
+
+/-
 TÁCTICA LINARITH
 -/
 def up_bounds (A : set ℝ) := { x : ℝ | ∀ a ∈ A, a ≤ x}
@@ -74,23 +104,110 @@ end
 /-
 TÁCTICA NLINARITH
 -/
+example (a b c d : ℤ)
+  (hba : b < a)
+  (hcb : c < b)
+  (hdc : d < c)
+  (h : a*c + b*d = (a + b - c + d) * (-a + b + c + d))
+  : a*c + b*d < a*b + c*d:=
+begin
+  nlinarith,
+end
+
+/-
+TÁCTICA ASSUME
+-/
+variables{n : ℕ}
+example : ∀ n, 3*n=n*3 :=
+begin
+  assume n,
+  sorry,
+end
+
+/-
+TÁCTICA BY_CONTRADICTION
+-/
+
+example (P Q : Prop) (h : ¬ Q → ¬ P) : P → Q :=
+begin
+  intro hP,
+  by_contradiction hnQ,
+  exact h hnQ hP,
+end
+
+/-
+TÁCTICA USE
+-/
+
+example : ∃ n : ℕ, 8 = 2*n :=
+begin
+  use 4,
+  refl, 
+end
+
+/-
+TÁCTICA INDUCTION
+-/
+
+example (t a b : ℕ) : t * (a + b) = t * a + t * b :=
+begin
+induction b with k h,
+{sorry,},
+
+{sorry,},
+end
 
 
 
 /-
-TÁCTICA APPLY
+TÁCTICA CASES
 -/
-def non_decreasing (f : ℝ → ℝ) := ∀ x₁ x₂, x₁ ≤ x₂ → f x₁ ≤ f x₂
-def non_increasing (f : ℝ → ℝ) := ∀ x₁ x₂, x₁ ≤ x₂ → f x₁ ≥ f x₂
-
-example (f g : ℝ → ℝ) (hf : non_decreasing f) (hg : non_increasing g) : 
-non_increasing (g ∘ f) :=
+example {a b : ℝ} : (0 ≤ a ∧ 0 ≤ b) → 0 ≤ a + b :=
 begin
-  intros x1 x2 h,
-  apply hg,
-  apply hf,
-  exact h,
+  intros hyp,
+  cases hyp with ha hb,
+  exact add_nonneg ha hb,
 end
+
+/-
+TÁCTICA SPLIT
+-/
+example (P Q R : Prop) : (P ∧ Q → R) ↔ (P → (Q → R)) :=
+begin
+  split, 
+  {intros h1 h2 h3,
+  exact h1 ⟨ h2,h3⟩ },
+  {intros h1 h2,
+  cases h2 with p q,
+  exact h1 p q}
+end
+
+example (P Q : Prop) (hp: P) (hq : Q) : P ∧ Q :=
+begin
+  split, 
+  {exact hp,},
+  {exact hq,},
+end
+
+
+/-
+TÁCTICA LIBRARY SEARCH
+-/
+example (a b : ℝ): 0 ≤ a → b ≤ a + b :=
+begin
+  library_search,
+end
+
+example (a b : ℝ): 0 ≤ a → b ≤ a + b :=
+begin
+  exact le_add_of_nonneg_left,
+end
+
+/-
+TÁCTICA REFINE
+-/
+
+
 
 /-
 TÁCTICA RING
